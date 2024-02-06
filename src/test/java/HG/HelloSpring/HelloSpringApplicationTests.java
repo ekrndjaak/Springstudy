c114ca6dcf104041f4ca8aa369c6925c53767d3b
dlagksrb_00 <dlagksrb0430@naver.com>
2024-02-06 11:32:18 +0900

    0206 study 초기화

---
+++ src/test/java/HG/HelloSpring/HelloSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package HG.HelloSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
