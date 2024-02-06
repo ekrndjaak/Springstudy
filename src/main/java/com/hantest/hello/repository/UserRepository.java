066d9ace8e18d91aa56762f18ca09b27fb6024d2
dlagksrb_00 <dlagksrb0430@naver.com>
2024-02-06 11:32:03 +0900

    0206 study 초기화

--- src/main/java/com/hantest/hello/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hantest.hello.repository;
-
-import com.hantest.hello.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsernameAndPassword(String username, String password);
-}
-
