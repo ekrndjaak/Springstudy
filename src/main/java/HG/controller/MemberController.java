a5d1b5e70e3c6d26ba0d5d3369f35908c9f9692e
dlagksrb_00 <dlagksrb0430@naver.com>
2024-03-19 19:47:58 +0900

    0319 clone

--- src/main/java/HG/controller/MemberController.java
+++ src/main/java/HG/controller/MemberController.java
@@ -1,14 +1,43 @@
 package HG.controller;
 
 
+import HG.domain.Member;
 import HG.service.MemberService;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
 @Controller
 public class MemberController {
     private final MemberService memberService;
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+    @PostMapping("/members/new")
+    public String create(@NotNull MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members",members);
+        return "members/memberList";
     }
 }
